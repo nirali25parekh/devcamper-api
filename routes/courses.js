49d46aaacafcbf3f2cf91fa972d1bb7af4b53a06
Nirali Parekh <nirali25parekh@gmail.com>
2020-04-11 00:39:06 +0530

    Crud on courses complete

--- routes/courses.js
+++ routes/courses.js
@@ -2,14 +2,27 @@ const express = require('express')
 
 const {
     getCourses,
+    getCourse,
+    addCourse,
+    updateCourse,
+    deleteCourse
 } = require('../controllers/courses')
 
 //mergeParams <= because 1 route also in bootcamps file
-const router = express.Router({ mergeParams: true}) 
+const router = express.Router({ mergeParams: true })
 
 // base url => '/api/v1/bootcamps' app.use() middleware in server.js
 router
     .route('/')
     .get(getCourses)
+    .post(addCourse)
+
+
+router
+    .route('/:id')
+    .get(getCourse)
+    .put(updateCourse)
+    .delete(deleteCourse)
     
+
 module.exports = router
